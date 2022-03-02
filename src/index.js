cb72dc5cc15f9e2c3cbe68bf633ad142c5699d8a
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2022-03-02 23:46:00 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+//Import CSS
+import "./index.css";
+
+import logo from "../resources/note-taking.png";
+
+//Append logo
+
+let img = document.createElement("img");
+img.src = logo;
+img.setAttribute("class", "logoImage");
+document.querySelector(".logo").appendChild(img);
+
+//Append title
+let h1 = document.createElement("h1");
+h1.innerText = "Note Taking App";
+h1.setAttribute("class", "header");
+document.querySelector(".logo").append(h1);
+
+document.querySelector("#addButtons").addEventListener("click", addNotes);
+
+let notesArr = JSON.parse(localStorage.getItem("notes")) || [];
+appendNotes(notesArr);
+
+function addNotes() {
+  //Get value of text content
+  let value = document.querySelector("#text-area").value;
+
+  value = value.trim();
+  if (value == "") {
+    alert("Please Write Something");
+  } else {
+    let obj = {
+      text: value,
+    };
+    notesArr.push(obj);
+    localStorage.setItem("notes", JSON.stringify(notesArr));
+    appendNotes(notesArr);
+  }
+}
+
+function appendNotes(notesArr) {
+  document.querySelector("#appendNotes").innerHTML = "";
+  notesArr.map((el) => {
+    let { text } = el;
+
+    let maindiv = document.createElement("div");
+    let firstDiv = document.createElement("div");
+    let p = document.createElement("p");
+    let button = document.createElement("button");
+
+    p.textContent = text;
+    button.textContent = "Delete Notes";
+
+    firstDiv.append(p);
+    maindiv.append(firstDiv, button);
+    document.querySelector("#appendNotes").append(maindiv);
+  });
+}
+
+//Creating Cards
+// const maindiv = document.createElement("div");
+// const firstDiv = document.createElement("div");
+// const p = document.createElement("p");
+// const button = document.createElement("button");
